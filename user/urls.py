04ddea2627517f00b54e4246a1ecabb5fdb29faa
davincikab <davidnganganjeri079@gmail.com>
2020-11-03 07:40:27 +0300

    updated user urls and views

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path
+from .views import home, register, faq_view, contacts_page
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-
+    path("", home, name="home"),
+    path("register/", register, name="register"),
+    path('contacts/', contacts_page, name='contacts'),
+    path('faqs/', faq_view, name='faq-view'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- user/views.py
+++ user/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "user/base.html")
+    return render(request, "base.html")
 
 def register(request):
-    return render(request, "user/")
+    return render(request, "user/account/register.html")
+
+def faq_view(request):
+    return render(request, "faq_views.html")
+
+def contacts_page(request):
+    return render(request, "contacts.html")
